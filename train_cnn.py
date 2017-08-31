ace575e788991e34edb1a6bd87b10457017e3153
mikeszabi <szabolcs.mike@gmail.com>
2017-08-31 12:59:31 +0000

    last train settings

--- train_cnn.py
+++ train_cnn.py
@@ -237,7 +237,7 @@ def train_and_evaluate(reader_train, reader_test, max_epochs, model_func):
 reader_train = create_reader(train_map, data_mean_file, True)
 reader_test  = create_reader(test_map, data_mean_file, False)
 
-pred = train_and_evaluate(reader_train, reader_test, max_epochs=1200,
+pred = train_and_evaluate(reader_train, reader_test, max_epochs=800,
                           model_func=create_basic_model)
 #pred_batch= train_and_evaluate(reader_train, reader_test, max_epochs=10, model_func=create_basic_model_with_batch_normalization)
 
