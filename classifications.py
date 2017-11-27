SU_Classifier fixes - char size, normalization, exlude holograms

classifications.py
@@ -28,20 +28,22 @@ def create_image(image_file,cropped=True,pad_rate=0.25,save_file='',category='')
     return img_square, char_sizes
 
 class cnn_classification:
-    def __init__(self,model_file=None,image_height = 64, image_width  = 64,im_mean=None):
+    def __init__(self,model_file=None,im_height = 64, im_width  = 64,im_mean=None):
         # model specific parameters
-        self.image_height=image_height # ToDo: parameter
-        self.image_width=image_width # ToDo: parameter
+        self.im_height=im_height # ToDo: parameter
+        self.im_width=im_width # ToDo: parameter
 
         self.im_mean=im_mean
         #self.model_name='cnn_model.dnn'
         #model_file=os.path.join(self.param.model_dir,self.model_name)
         print('...loading classification model')
+        # ToDo: do checks for image size and num_channel
+        
         self.pred=load_model(model_file)
     
     def classify(self, img, char_sizes=None):
         
-        if char_sizes:
+        if char_sizes is not None:
             maxl=char_sizes[0].astype('float32')
             minl=char_sizes[1].astype('float32')
    
@@ -53,7 +55,7 @@ class cnn_classification:
             im=img.copy()
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            data = img_as_ubyte(resize(im, (self.image_height,self.image_width), order=1))
+            data = img_as_ubyte(resize(im, (self.im_height,self.im_width), order=1))
         if data.ndim==3: 
             rgb_image=data.astype('float32')
             if self.im_mean:
@@ -64,7 +66,7 @@ class cnn_classification:
             gray_image=data.astype('float32')
             pic = np.ascontiguousarray(np.expand_dims(gray_image, axis=0)) # CHW format
        
-        if char_sizes:    
+        if char_sizes is not None:    
             result  = np.round(np.squeeze(self.pred.eval({self.pred.arguments[0]:[pic],
                                              self.pred.arguments[1]:[[maxl,minl]]}))*100)
         else:

crop.py
@@ -22,7 +22,7 @@ from matplotlib import pyplot as plt
 import matplotlib.patches as patches
 
 from skimage.color import rgb2gray
-from skimage.feature import blob_dog
+#from skimage.feature import blob_dog
 
 import numpy as np
 
@@ -38,7 +38,7 @@ def crop_edge(gray,pixel_per_micron=1.5):
     edges2 = morphology.binary_dilation(edges1,morphology.disk(11))
  
     bb=(0,0,gray.shape[0],gray.shape[1])
-    char_sizes=(max(gray.shape)/pixel_per_micron,min(gray.shape)/pixel_per_micron)
+    char_sizes=np.asarray((max(gray.shape)/pixel_per_micron,min(gray.shape)/pixel_per_micron))
     
     label_im=measure.label(edges2)
     props = measure.regionprops(label_im,intensity_image=1-gray)
@@ -50,8 +50,8 @@ def crop_edge(gray,pixel_per_micron=1.5):
         if prop_large.major_axis_length>min_extent_in_micron*pixel_per_micron:
             # min length in micron
             bb=prop_large.bbox
-            char_sizes=(prop_large.major_axis_length/pixel_per_micron,
-                        prop_large.minor_axis_length/pixel_per_micron)
+            char_sizes=np.asarray((prop_large.major_axis_length/pixel_per_micron,
+                        prop_large.minor_axis_length/pixel_per_micron))
 
     return bb,char_sizes
 